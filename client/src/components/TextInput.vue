4b087cc117aedf0405987854c7b6445aa40a8c95
Lutreze Hue Jacinto <jacintolutrezehue@gmail.com>
2025-03-10 12:48:18 +0800

    update text input

--- client/src/components/TextInput.vue
+++ client/src/components/TextInput.vue
@@ -4,7 +4,7 @@ import { onMounted, ref } from 'vue';
 const props = defineProps({
     type: {
         type: String,
-        default: 'text' 
+        default: 'text'
     },
     modelValue: {
         type: String,
@@ -12,7 +12,8 @@ const props = defineProps({
     },
     id: String,
     required: Boolean,
-    autocomplete: String
+    autocomplete: String,
+    class: String
 });
 
 const emit = defineEmits(['update:modelValue']);
@@ -34,28 +35,22 @@ const updateValue = (event) => {
 </script>
 
 <template>
-    <input :id="id" :type="props.type" <!-- Use the type prop dynamically -->
-    :class="[
-    'w-full',
-    'text-gray-800',
-    'text-sm',
-    'border',
-    'border-gray-300',
-    'px-4',
-    'py-3',
-    'rounded-md',
-    'focus:border-gray-200',
-    'focus:outline-none',
-    'focus:ring-2',
-    'focus:ring-offset-2',
-    'focus:ring-gray-300',
-    'transition-colors',
-    'duration-300'
-    ]"
-    :value="modelValue"
-    :required="required"
-    :autocomplete="autocomplete"
-    @input="updateValue"
-    ref="input"
-    />
+    <input :id="id" :type="type" :class="[
+        props.class || '',
+        'w-full',
+        'text-gray-800',
+        'text-sm',
+        'border',
+        'border-gray-300',
+        'px-4',
+        'py-3',
+        'rounded-md',
+        'focus:border-gray-200',
+        'focus:outline-none',
+        'focus:ring-2',
+        'focus:ring-offset-2',
+        'focus:ring-gray-300',
+        'transition-colors',
+        'duration-300'
+    ]" :value="modelValue" :required="required" :autocomplete="autocomplete" @input="updateValue" ref="input" />
 </template>
