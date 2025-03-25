776a22173dc6a7f768092d3b96064afe1826a447
Lutreze Hue Jacinto <jacintolutrezehue@gmail.com>
2025-03-25 11:44:26 +0800

    refactor: update CORS configuration and simplify Vercel routing

--- backend/config/cors.js
+++ backend/config/cors.js
@@ -1,5 +1,6 @@
 const corsOptions = {
-    origin: ['http://localhost:5173', 'http://localhost:7777'],
+    // origin: ['http://localhost:5173', 'http://localhost:7777'],
+    origin: ['http://localhost:7777', 'https://payroll-system-ten.vercel.app/'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization', 'user-role'],
     credentials: true,
