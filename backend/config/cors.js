fe7a4b55f18c1e3e12da248bea7242438166e955
Lutreze Hue Jacinto <86185117+nocturnalDev11@users.noreply.github.com>
2025-03-25 06:11:03 +0000

    commit

--- backend/config/cors.js
+++
@@ -1,6 +0,0 @@
-export const corsOptions = {
-    // origin: 'http://localhost:5174',
-    origin: 'http://localhost:5173',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-};
