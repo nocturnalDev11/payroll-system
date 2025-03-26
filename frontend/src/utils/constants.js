68cc53b745e5fb14fda80818ac751b188f4a1c8f
Lutreze Hue Jacinto <jacintolutrezehue@gmail.com>
2025-03-26 11:41:18 +0800

    Add logging for API URL constants in constants.js

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,1 +1,3 @@
+console.log('VITE_APP_API_URL:', process.env.VITE_APP_API_URL);
 export const BASE_API_URL = process.env.VITE_APP_API_URL || 'https://payroll-system-api-topaz.vercel.app';
+console.log('BASE_API_URL:', BASE_API_URL);
