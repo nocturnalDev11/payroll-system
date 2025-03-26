8f91d0ae29817137176cdb2f45d74b7ffbd9b350
Lutreze Hue Jacinto <jacintolutrezehue@gmail.com>
2025-03-26 11:44:38 +0800

    Remove .env file and update BASE_API_URL to a hardcoded value in constants.js

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,3 +1,2 @@
-console.log('VITE_APP_API_URL:', process.env.VITE_APP_API_URL);
-export const BASE_API_URL = process.env.VITE_APP_API_URL || 'https://payroll-system-api-topaz.vercel.app';
+export const BASE_API_URL = 'https://payroll-system-api-topaz.vercel.app';
 console.log('BASE_API_URL:', BASE_API_URL);
