a72b989ddd91583d1082d566e7d2140e5e84d51f
Lutreze Hue Jacinto <jacintolutrezehue@gmail.com>
2025-03-26 13:06:12 +0800

    dasdsad

--- backend/config/cors.js
+++ backend/config/cors.js
@@ -1,9 +1,5 @@
 const corsOptions = {
-    origin: [
-        'https://payroll-system-frontend-pied.vercel.app',
-        'https://payroll-system-api-topaz.vercel.app',
-        'http://localhost:5173'
-    ],
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization', 'user-role'],
     credentials: true,
