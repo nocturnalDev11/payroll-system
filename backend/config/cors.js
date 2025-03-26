c1b546c806704d60a17fa6691269ff1f0e558015
Lutreze Hue Jacinto <jacintolutrezehue@gmail.com>
2025-03-26 09:53:26 +0800

    Update CORS configuration to restrict origins and export options

--- backend/config/cors.js
+++ backend/config/cors.js
@@ -1,7 +1,11 @@
 const corsOptions = {
-    origin: '*',
+    origin: [
+        'https://payroll-system-frontend-orpin.vercel.app',
+        'http://localhost:5173'
+    ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization', 'user-role'],
     credentials: true,
     optionsSuccessStatus: 200
 };
+module.exports = corsOptions;
