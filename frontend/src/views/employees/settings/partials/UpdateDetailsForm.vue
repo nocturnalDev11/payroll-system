d4b31a9d51e168b8e86515ad16c311cd21068284
Lutreze Hue Jacinto <jacintolutrezehue@gmail.com>
2025-04-09 20:00:37 +0800

    feat: Remove dark mode classes from UpdateDetailsForm for improved consistency

--- frontend/src/views/employees/settings/partials/UpdateDetailsForm.vue
+++ frontend/src/views/employees/settings/partials/UpdateDetailsForm.vue
@@ -87,8 +87,8 @@ const updateEmployee = async () => {
 <template>
     <div>
         <header>
-            <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">Profile Information</h2>
-            <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">Update your profile</p>
+            <h2 class="text-lg font-medium text-gray-900">Profile Information</h2>
+            <p class="mt-1 text-sm text-gray-600">Update your profile</p>
         </header>
         <form @submit.prevent="updateEmployee" class="mt-6 space-y-6">
             <div class="grid grid-cols-1 sm:grid-cols-2 gap-6">
